63264d7f3af7723bf2f9fdd22a54535f9be3e688
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-02-08 14:10:29 -0800

    Load ontology from sandbox-api.smartplatforms.org by default

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,4 @@
 ant -lib ../lib-codegen -lib ../lib  -f build-with-ivy.xml  \
-   -Dsmart_owl_path="/home/nate/SMArt/smart_server/smart/document_processing/schema/smart.owl"  \
-   -Djust_for_challenge="challenge"
+   -Djust_for_challenge="challenge" \
+   -Dsmart_owl_path=smart.owl \
+   -Dontology_location="http://sandbox-api.smartplatforms.org/ontology"
